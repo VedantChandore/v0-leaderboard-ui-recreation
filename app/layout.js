aa830afe19f29b26cb0b0f023ee2f465f067b4c2
vedant <vedantchandore5@gmail.com>
2025-11-12 19:11:11 +0530

    authentication removed

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/next'
-import { AuthProvider } from '../contexts/AuthContext'
 import './globals.css'
 
 export const metadata = {
@@ -14,9 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        {children}
         <Analytics />
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
 import { Leaderboard } from "../components/leaderboard"
-import { ProtectedRoute } from "../components/ProtectedRoute"
 
 export default function Page() {
-  return (
-    <ProtectedRoute>
-      <Leaderboard />
-    </ProtectedRoute>
-  )
+  return <Leaderboard />
 }
